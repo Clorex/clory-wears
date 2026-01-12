def195b2b5d7d6b9dd29d3854678519dc05da6b6
Itabita Miracle <itabitamiracle090@gmail.com>
2026-01-12 15:10:40 -0800

    Move home page out of route group to fix Vercel build

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,15 @@
-export { default } from "./(store)/page";
+import SiteHeader from "./_components/SiteHeader";
+import SiteFooter from "./_components/SiteFooter";
+import HomePage from "./(store)/HomePage";
+
+export default function Page() {
+  return (
+    <>
+      <SiteHeader />
+      <main style={{ minHeight: "72vh" }}>
+        <HomePage />
+      </main>
+      <SiteFooter />
+    </>
+  );
+}
